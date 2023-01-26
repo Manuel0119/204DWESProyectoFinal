700c8a7bf5a493ca8b85a3f0d1c4c30c2fd74183
Manuel Martin <manuel.maralo.2@educa.jcyl.es>
2023-01-26 09:43:29 +0100

    26-01-2023 Proyecto Final con mejoras

--- controller/cDetalle.php
+++ controller/cDetalle.php
@@ -2,6 +2,7 @@
 if(isset($_REQUEST['volver'])){
     $_SESSION['paginaEnCurso']=$_SESSION['paginaAnterior'];
     header('Location: index.php');
+    exit();
 }
 require_once $aVistas['layout'];
 ?>
--- controller/cInicioPrivado.php
+++ controller/cInicioPrivado.php
@@ -1,14 +1,16 @@
 <?php
 if (isset($_REQUEST['salir'])){
     $_SESSION['paginaEnCurso']='inicioPublico';
-    $_SESSION['User204DWESLoginLogoff']=null;
+    $_SESSION['User204DWESProyectoFinal']=null;
     session_destroy();
     header("Location: index.php"); 
+    exit();
 }
 if(isset($_REQUEST['detalle'])){
     $_SESSION['paginaEnCurso']='detalle';
     $_SESSION['paginaAnterior']='inicioPrivado';
     header("Location: index.php"); 
+    exit();
 }
 require_once $aVistas['layout'];
 
